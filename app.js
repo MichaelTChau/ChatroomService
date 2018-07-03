e539f891f7acea86e5c85d758d3447ca77313695
Michael T Chau <mtchau@uwaterloo.ca>
2018-07-02 20:04:07 -0400

    Added signup, login, create chatroom, load chatroom endpoints

--- app.js
+++ app.js
@@ -83,16 +83,63 @@ app.post('/login', function(req, response) {
 });
 
 app.get("/chatrooms",function(request, response){
-    var obj = { id : "Wow", Content : "content " +"cool" };
+  db.collection("chatrooms").find().project({ _id: 0, messages:0}).toArray(function(err, res) {
+    console.log(res);
+    var content , error, sessionToken, type, message, code;
+    if (err) throw err;
+    error = false;
+    type = "Success";
+    message ="Chatroom loaded success!";
+    code =  200;
+    var obj =  { status : { type: type, message:message,code:code,error:error},
+    data:{ chatrooms: res}} ;
     response.writeHead(200, {"Content-Type": "application/json"});
     response.write(JSON.stringify(obj));
+  });
 });
 
 app.post("/createChatroom",function(request, response){
-    var obj = { id : "Wow", Content : "content " +"cool" };
+    var chatroomName = request.body.chatroomName;
+    var roomExist = { chatroomName : chatroomName};
+    var roomTaken = false;
+    db.collection("chatrooms").find(roomExist).toArray(function(err, res) {
+      console.log(res);
+      if (err) throw err;
+      if(res.length >= 1) roomTaken = true;
+      makeRoom(chatroomName,roomTaken,response);
+    });
+});
+
+function makeRoom(chatroomName,roomTaken,response){
+  if(roomTaken){
+    var obj =  { status : { type: "Bad request", message:"Error the room exist already",code:201,error:true} };
     response.writeHead(200, {"Content-Type": "application/json"});
     response.write(JSON.stringify(obj));
+    return;
+  }
+  var date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+  var newRoom = {chatroomName:chatroomName,dateCreated:date, messages:[]};
+  db.collection("chatrooms").insertOne(newRoom, function(err, res) {
+    if (err) throw err;
+    var obj = { status : { type: "Success", message:"Room creation is a success",code:200,error:false} };;
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.write(JSON.stringify(obj));
+    console.log("success!");
+  });
+}
+
+app.post("/getRoomData",function(request, response){
+    var chatroomName = request.body.chatroomName;
+    var roomExist = { chatroomName : chatroomName};
+    db.collection("chatrooms").find(roomExist).project({_id:0}).toArray(function(err, res) {
+      console.log(res);
+      if (err) throw err;
+      if(res.length !==0){
+
+      }
+    });
 });
+
 
 //Socket setup
 var io = socket(server);
